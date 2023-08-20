96d974519d013b36bc571755e004a5c732523a6e
karan jain <karanjain2212@gmail.com>
2023-08-20 14:30:25 +0530

    experence vertical line

--- src/HOC/SectionWrapper.jsx
+++ src/HOC/SectionWrapper.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
 import { styles } from "../style";
 import { staggerContainer } from "../utils/motion";
 
 const SectionWrapper = (Component, idName) =>
   function HOC(props) {
-    const [ref, inView] = useInView(); // Track if the component is in view
-
     return (
       <motion.section
+        // varients={staggerContainer()}
         initial="hidden"
-        animate={inView ? "show" : "hidden"} // Animate based on inView status
+        whileInView="show"
+        viewport={{ once: true, amount: 0.35 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-        ref={ref} // Attach the ref to the section element
       >
-        <span className="hast-span" id={idName}>
-          &nbsp;
-        </span>
         <Component {...props} />
       </motion.section>
     );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { styles } from "../style";
+import { motion } from "framer-motion";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../HOC";
+import { textVariant } from "../utils/motion";
+import "react-vertical-timeline-component/style.min.css";
+
+const ExperienceCard = ({ experiences }) => (
+  <VerticalTimelineElement
+    contentStyle={{ background: "#1d1836", color: "#fff" }}
+    contentArrowStyle={{ borderRight: "7px solid #232631" }}
+    date={experiences.date}
+    iconStyle={{ background: experiences.iconBg }}
+    icon={
+      <div className="flex justify-center items-center w-full h-full">
+        <img
+          src={experiences.icon}
+          alt={experiences.company_name}
+          className="w-[60%] h-[60%] object-contain"
+        />
+      </div>
+    }
+  >
+    <div className="text-white font-bold text-[24px]">{experiences.title}</div>
+  </VerticalTimelineElement>
+);
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experiences.</h2>
+      </motion.div>
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experiences={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
 
-export default Experience
+export default SectionWrapper(Experience, "Work");
