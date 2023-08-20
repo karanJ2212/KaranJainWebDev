b61ee45c5ef0f407d80eca8ef352aa74ed06efc1
karan jain <karanjain2212@gmail.com>
2023-08-20 14:47:59 +0530

    details

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -26,7 +26,25 @@ const ExperienceCard = ({ experiences }) => (
       </div>
     }
   >
-    <div className="text-white font-bold text-[24px]">{experiences.title}</div>
+    <div className="text-white font-bold text-[24px]">
+      <h3> {experiences.title} </h3>
+      <p
+        className="text-secondary text-[]16px] text-semibold"
+        style={{ margin: 0 }}
+      >
+        {experiences.company_name}
+      </p>
+    </div>
+    <ul className="mt-5 list-disc ml-5 space-y-2">
+      {experiences.points.map((point, index) => (
+        <li
+          key={`experience-point-${index}`}
+          className="text-white-100 text-[14px] pl-1 tracking-wider"
+        >
+          {point}
+        </li>
+      ))}
+    </ul>
   </VerticalTimelineElement>
 );
 
