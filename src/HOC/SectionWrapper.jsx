be59c0764f5fd2fc2526386964e8fa1fe89504e1
karan jain <karanjain2212@gmail.com>
2023-08-20 11:23:32 +0530

    overview

--- src/HOC/SectionWrapper.jsx
+++ src/HOC/SectionWrapper.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { styles } from "../style";
 import { staggerContainer } from "../utils/motion";
 
 const SectionWrapper = (Component, idName) =>
   function HOC(props) {
+    const [ref, inView] = useInView(); // Track if the component is in view
+
     return (
-      <motion.section>
+      <motion.section
+        initial="hidden"
+        animate={inView ? "show" : "hidden"} // Animate based on inView status
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+        ref={ref} // Attach the ref to the section element
+      >
+        <span className="hast-span" id={idName}>
+          &nbsp;
+        </span>
         <Component {...props} />
       </motion.section>
     );
