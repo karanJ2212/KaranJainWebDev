627886241bd87e937adb21b6e4854fd799114693
karan jain <karanjain2212@gmail.com>
2023-09-13 20:38:56 +0530

    removed ball canvas from small screen

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -14,54 +14,87 @@ const Tech = () => {
   return (
     <div className="flex flex-col gap-y-10">
       {width < 768 ? (
-        <div>
-          <p className={`${styles.sectionSubText} `}>My Skills</p>
-          <h2 className={`${styles.sectionHeadText}`}>Technologies</h2>
-        </div>
-      ) : (
-        <motion.div variants={textVariant()}>
-          <p className={`${styles.sectionSubText} `}>My skills</p>
-          <h2 className={`${styles.sectionHeadText}`}>Technologies.</h2>
-        </motion.div>
-      )}
-
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
-        {skills.map((skillGroup) => (
-          <Tilt className="xs:w-[250px] w-full" key={skillGroup.id}>
-            <motion.div
-              className="w-full rounded-[20px] green-pink-gradient p-[2px] shadow-card hover:shadow-xl transform transition-transform duration-300 ease-in-out"
-              options={{ max: 45, scale: 1, speed: 450 }}
-            >
-              <div className="flex flex-col bg-tertiary rounded-[50px] p-4 h-full cursor-default">
-                <h3 className="text-white text-[20px] font-bold text-center">
-                  {skillGroup.title}
-                </h3>
-                <div className="flex flex-wrap justify-center gap-2 mt-2 text-center">
-                  {skillGroup.skills.map((category) =>
-                    category.skills.map((skill) => (
-                      <p
-                        key={skill}
-                        className={`text-white ${category.color} `}
-                      >
-                        {skill}
-                      </p>
-                    ))
-                  )}
-                </div>
-              </div>
-            </motion.div>
-          </Tilt>
-        ))}
-      </div>
-
-      <div className="flex flex-row flex-wrap justify-center gap-10 text-center">
-        {technologies.map((tech) => (
-          <div className="w-28 h-28" key={tech.name}>
-            <BallCanvas icon={tech.icon} />
-            <p className={styles.sectionSubText}>{tech.name}</p>
+        <>
+          <div>
+            <p className={`${styles.sectionSubText} `}>My Skills</p>
+            <h2 className={`${styles.sectionHeadText}`}>Technologies</h2>
           </div>
-        ))}
-      </div>
+
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
+            {skills.map((skillGroup) => (
+              <Tilt className="xs:w-[250px] w-full" key={skillGroup.id}>
+                <motion.div
+                  className="w-full rounded-[20px] green-pink-gradient p-[2px] shadow-card hover:shadow-xl transform transition-transform duration-300 ease-in-out"
+                  options={{ max: 45, scale: 1, speed: 450 }}
+                >
+                  <div className="flex flex-col bg-tertiary rounded-[50px] p-4 h-full cursor-default">
+                    <h3 className="text-white text-[20px] font-bold text-center">
+                      {skillGroup.title}
+                    </h3>
+                    <div className="flex flex-wrap justify-center gap-2 mt-2 text-center">
+                      {skillGroup.skills.map((category) =>
+                        category.skills.map((skill) => (
+                          <p
+                            key={skill}
+                            className={`text-white ${category.color} `}
+                          >
+                            {skill}
+                          </p>
+                        ))
+                      )}
+                    </div>
+                  </div>
+                </motion.div>
+              </Tilt>
+            ))}
+          </div>
+        </>
+      ) : (
+        <>
+          <motion.div variants={textVariant()}>
+            <p className={`${styles.sectionSubText} `}>My skills</p>
+            <h2 className={`${styles.sectionHeadText}`}>Technologies.</h2>
+          </motion.div>
+
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
+            {skills.map((skillGroup) => (
+              <Tilt className="xs:w-[250px] w-full" key={skillGroup.id}>
+                <motion.div
+                  className="w-full rounded-[20px] green-pink-gradient p-[2px] shadow-card hover:shadow-xl transform transition-transform duration-300 ease-in-out"
+                  options={{ max: 45, scale: 1, speed: 450 }}
+                >
+                  <div className="flex flex-col bg-tertiary rounded-[50px] p-4 h-full cursor-default">
+                    <h3 className="text-white text-[20px] font-bold text-center">
+                      {skillGroup.title}
+                    </h3>
+                    <div className="flex flex-wrap justify-center gap-2 mt-2 text-center">
+                      {skillGroup.skills.map((category) =>
+                        category.skills.map((skill) => (
+                          <p
+                            key={skill}
+                            className={`text-white ${category.color} `}
+                          >
+                            {skill}
+                          </p>
+                        ))
+                      )}
+                    </div>
+                  </div>
+                </motion.div>
+              </Tilt>
+            ))}
+          </div>
+
+          <div className="flex flex-row flex-wrap justify-center gap-10 text-center">
+            {technologies.map((tech) => (
+              <div className="w-28 h-28" key={tech.name}>
+                <BallCanvas icon={tech.icon} />
+                <p className={styles.sectionSubText}>{tech.name}</p>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
